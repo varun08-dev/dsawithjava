02857fb6249de64905bd5beba3bfa0767d4f41a9
varun08-dev <kumarvarun5656@gmail.com>
2024-12-18 18:17:05 +0530

    Add files via upload

---
+++ Day3.java
@@ -0,0 +1,24 @@
+public class Day3 {
+    
+        //...................LINKED LIST................
+       
+       static class Node{
+       
+         int data;
+         Node next;
+       
+       
+        Node(int val){
+          this.data=val;
+          this.next=null;
+       }
+       }
+       Node head=null;
+           public static void main(String [] args){
+          
+       
+           }
+           
+       
+       
+}
